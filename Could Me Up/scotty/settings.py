ae7e323fb207df4902724d229da2a02943f16467
Joao Mateus <joaodiogomateus868@gmail.com>
2026-03-10 19:36:33 +0000

    alterações de segurança

--- Could Me Up/scotty/settings.py
+++ Could Me Up/scotty/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+m80md01w&qxf%i=o$47p-lka!lwy!t1gq^$n7#agx5s_@ycdk'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = []
 
